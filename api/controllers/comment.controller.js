f3fae561a4dbf148da0e8e323268b6ddd0447251
Aneal07 <anilmaharjan07@outlook.com>
2024-05-30 15:00:00 +0100

    add onedit event

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,4 +1,4 @@
-import Comment from "../models/comment.model.js"
+import Comment from "../models/comment.model.js";
 
 export const createComment = async (req, res, next) => {
   try {
@@ -15,9 +15,9 @@ export const createComment = async (req, res, next) => {
       postId,
       userId,
     });
-    await newComment.save()
+    await newComment.save();
 
-    res.status(200).json(newComment)
+    res.status(200).json(newComment);
   } catch (error) {
     next(error);
   }
@@ -27,45 +27,44 @@ export const getPostComments = async (req, res, next) => {
   try {
     const comments = await Comment.find({ postId: req.params.postId }).sort({
       createdAt: -1,
-    })
-    res.status(200).json(comments)
+    });
+    res.status(200).json(comments);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const likeComment = async (req, res, next) => {
   try {
     const comment = await Comment.findById(req.params.commentId);
     if (!comment) {
-      return next(errorHandler(404, "Comment not found"))
+      return next(errorHandler(404, "Comment not found"));
     }
-    const userIndex = comment.likes.indexOf(req.user.id)
+    const userIndex = comment.likes.indexOf(req.user.id);
     if (userIndex === -1) {
-      comment.numberOfLikes += 1
-      comment.likes.push(req.user.id)
+      comment.numberOfLikes += 1;
+      comment.likes.push(req.user.id);
     } else {
-      comment.numberOfLikes -= 1
-      comment.likes.splice(userIndex, 1)
+      comment.numberOfLikes -= 1;
+      comment.likes.splice(userIndex, 1);
     }
     await comment.save();
-    res.status(200).json(comment)
+    res.status(200).json(comment);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
-
+};
 
 export const editComment = async (req, res, next) => {
   try {
-    const comment = await Comment.findById(req.params.commentId)
+    const comment = await Comment.findById(req.params.commentId);
     if (!comment) {
-      return next(errorHandler(404, "Comment not found"))
+      return next(errorHandler(404, "Comment not found"));
     }
     if (comment.userId !== req.user.id && !req.user.isAdmin) {
       return next(
         errorHandler(403, "You are not allowed to edit this comment")
-      )
+      );
     }
 
     const editedComment = await Comment.findByIdAndUpdate(
@@ -74,9 +73,9 @@ export const editComment = async (req, res, next) => {
         content: req.body.content,
       },
       { new: true }
-    )
+    );
     res.status(200).json(editedComment);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
--- client/src/components/ComponentSection.jsx
+++ client/src/components/ComponentSection.jsx
@@ -55,42 +55,41 @@ const ComponentSection = ({ postId }) => {
     getComments()
   }, [postId])
 
- const handleLike = async commentId => {
-  try {
-    if (!currentUser) {
-      navigate('/sign-in')
-      return
-    }
-    const res = await fetch(`/api/comment/likeComment/${commentId}`, {
-      method: 'PUT'
-    })
-    if (res.ok) {
-      const data = await res.json()
-      setComments(
-        comments.map(comment =>
-          comment._id === commentId
-            ? {
-                ...comment,
-                likes: data.likes,
-                numberOfLikes: data.likes.length
-              }
-            : comment
+  const handleLike = async commentId => {
+    try {
+      if (!currentUser) {
+        navigate('/sign-in')
+        return
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: 'PUT'
+      })
+      if (res.ok) {
+        const data = await res.json()
+        setComments(
+          comments.map(comment =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length
+                }
+              : comment
+          )
         )
-      )
+      }
+    } catch (error) {
+      console.log(error.message)
     }
-  } catch (error) {
-    console.log(error.message)
   }
-}
 
-const handleEdit = async (comment, editedContent) => {
-  setComments(
-    comments.map(c =>
-      c._id === comment._id ? { ...c, content: editedContent } : c
+  const handleEdit = async (comment, editedContent) => {
+    setComments(
+      comments.map(c =>
+        c._id === comment._id ? { ...c, content: editedContent } : c
+      )
     )
-  )
-}
-
+  }
 
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
@@ -155,7 +154,12 @@ const handleEdit = async (comment, editedContent) => {
             </div>
           </div>
           {comments.map(comment => (
-            <Comment key={comment._id} comment={comment} onLike = {handleLike} />
+            <Comment
+              key={comment._id}
+              comment={comment}
+              onLike={handleLike}
+              onEdit={handleEdit}
+            />
           ))}
         </>
       )}
