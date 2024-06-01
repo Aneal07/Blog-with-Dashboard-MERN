6406d55c6b988b282b95e4e472e6c715b9658bd2
Aneal07 <anilmaharjan07@outlook.com>
2024-06-01 08:11:27 +0100

    add comments section to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import { Sidebar } from 'flowbite-react'
 import { useEffect, useState } from 'react'
-import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from 'react-icons/hi'
+import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser, HiAnnotation } from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom'
 import { signoutSuccess } from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
@@ -39,17 +39,19 @@ const handleSignout = async () => {
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup className='flex flex-col gap-1'>
-            <Link to='/dashboard?tab=profile'>
-            <Sidebar.Item 
-            active={tab === 'profile'} 
-            icon={HiUser}
-             label={currentUser.isAdmin ? 'Admin' : 'User'}
-              labelColor='dark'
-                 as='button'
-              >
-                Profile
-            </Sidebar.Item>
-            </Link>
+          ;<>
+  <Link to='/dashboard?tab=users'>
+    <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div'>
+      Users
+    </Sidebar.Item>
+  </Link>
+  <Link to='/dashboard?tab=comments'>
+    <Sidebar.Item active={tab === 'comments'} icon={HiAnnotation} as='div'>
+      Comments
+    </Sidebar.Item>
+  </Link>
+</>
+
             {currentUser.isAdmin && (
           <Link to='/dashboard?tab=posts'>
               <Sidebar.Item
