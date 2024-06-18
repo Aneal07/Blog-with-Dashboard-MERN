5a82314636a8df0508b63dedee094e85cec49c41
Aneal07 <anilmaharjan07@outlook.com>
2024-06-18 12:37:23 +0100

    refactor the about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -10,17 +10,14 @@ const About = () => {
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Mighty's Blog! This blog was created by Mighty as a
-              personal project to share his thoughts and ideas with the world.
-              Mighty is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+              Welcome to Mighty's Blog! This blog was created by as a
+              personal project to share thoughts and ideas with the world.
             </p>
 
             <p>
               On this blog, you'll find weekly articles and tutorials on topics
               such as web development, software engineering, and programming
-              languages. Mighty is always learning and exploring new
-              technologies, so be sure to check back often for new content!
+              languages. 
             </p>
 
             <p>
