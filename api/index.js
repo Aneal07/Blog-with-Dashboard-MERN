03546655cd4866ae53b8b0078bd94620bba399c5
Aneal07 <anilmaharjan07@outlook.com>
2024-06-18 12:53:56 +0100

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
 import postRoutes from './routes/post.route.js'
 import commentRoute from './routes/comment.route.js'
+import path from 'path'
 
 
 dotenv.config()
@@ -21,6 +22,7 @@ mongoose.connect
       console.log(err)  
     })
 
+    const __dirname = path.resolve()
 
 const app = express()
 
@@ -36,6 +38,12 @@ app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comment', commentRoute)
+
+app.use(express.static(path.join(__dirname, "/client/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
